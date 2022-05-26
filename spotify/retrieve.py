89f0e6583d7e9e73e7028610036214722c837032
zacharygilliom <zacharygilliom@gmail.com>
2022-05-26 00:19:00 -0400

    initial commit, added initial files to test out api is working.

---
+++ spotify/retrieve.py
@@ -0,0 +1,16 @@
+import spotipy
+import sys
+sys.path.append('/home/zach/programming/python/SpotifyUsage/internal')
+import creds 
+from spotipy.oauth2 import SpotifyClientCredentials
+
+if __name__ == '__main__':
+    CLIENT_ID, CLIENT_SECRET = creds.get_client_credentials()
+    
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET))
+    
+    results = sp.search(q='weezer', limit=20)
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx, track['name'])
+    
