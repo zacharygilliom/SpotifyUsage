ce04975b4e634272023a70a869e530c02e55db1e
zacharygilliom <zacharygilliom@gmail.com>
2022-05-30 01:15:37 -0400

    Added database class with connection, query, and insert statements.  Added DB creds

--- spotify/retrieve.py
+++ spotify/retrieve.py
@@ -5,22 +5,69 @@ import creds
 from spotipy.oauth2 import SpotifyClientCredentials
 import psycopg
 
-def connect():
-    with psycopg.connect("dbname=spotifyusage user=postgres") as conn:
-        with conn.cursor() as cur:
-            cur.execute("""
-                CREATE TABLE IF NOT EXISTS songs (
-                    name varchar(20))
-                """)
-            cur.execute("""
-                CREATE TABLE IF NOT EXISTS title (
-                    name varchar(10))
-                """)
-            conn.commit()
+class Database:
 
+    def __init__(self, dbname, user, password, host, port):
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
 
+    def create_tables(self):
+        params = {
+                'dbname': self.dbname,
+                'user': self.user,
+                'password': self.password,
+                'host': self.host,
+                'port': self.port 
+        }
+        with psycopg.connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS songs (
+                        id serial PRIMARY KEY,
+                        name varchar(20),
+                        band varchar(20))
+                    """)
+                conn.commit()
+                conn.close()
+
+    def query(self):
+        params = {
+                'dbname': self.dbname,
+                'user': self.user,
+                'password': self.password,
+                'host': self.host,
+                'port': self.port 
+        }
+        with psycopg.connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM songs")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+                conn.commit()
+                conn.close()
+
+    def insert(self):
+        params = {
+                'dbname': self.dbname,
+                'user': self.user,
+                'password': self.password,
+                'host': self.host,
+                'port': self.port 
+        }
+        with psycopg.connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO songs (name,band) VALUES(%s,%s)",
+                    ("Little Dark Age", "MGMT"))
+                conn.commit()
+                conn.close()
+        
 if __name__ == '__main__':
     CLIENT_ID, CLIENT_SECRET = creds.get_client_credentials()
+    DBNAME, USER, PASSWORD, HOST, PORT = creds.get_db_credentials()    
     
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET))
@@ -28,7 +75,10 @@ if __name__ == '__main__':
     results = sp.search(q='weezer', limit=20)
     for idx, track in enumerate(results['tracks']['items']):
         print(idx, track['name'])
-    
-    connect()
+   
+    db = Database(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
+    db.create_tables()
+    db.insert()
+    db.query()
 
 
