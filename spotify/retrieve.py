48f45d2b0de1ee6d2a9b20e6a83ebcf1f87cecc2
zacharygilliom <zacharygilliom@gmail.com>
2022-05-31 23:29:20 -0400

    Added two more tables to database initialization, added functions to call recently played tracks and to get the audio features of those tracks.

--- spotify/retrieve.py
+++ spotify/retrieve.py
@@ -24,7 +24,28 @@ class Database:
                     CREATE TABLE IF NOT EXISTS songs (
                         id serial PRIMARY KEY,
                         name varchar(20),
-                        band varchar(20))
+                        band varchar(20),
+                        spotify_id varchar(22))
+                    """)
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS played (
+                        song_id integer references songs(id),
+                        played_at timestamp)
+                    """)
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS audio_features (
+                        song_id integer references songs(id),
+                        danceability real,
+                        energy real,
+                        key integer,
+                        loudness real,
+                        mode int,
+                        speechiness real,
+                        acousticness real,
+                        instruementalness real,
+                        liveness real,
+                        valence real,
+                        tempo real)
                     """)
                 conn.commit()
                 conn.close()
@@ -38,6 +59,7 @@ class Database:
                     print(row)
                 conn.commit()
                 conn.close()
+                return rows
 
     def insert(self, song, band):
         with psycopg.connect(**self.params) as conn:
@@ -53,27 +75,42 @@ class Database:
                 cur.execute(f"DROP TABLE {table}")
                 conn.commit()
                 conn.close()
-        
+
+def get_recently_played_track_ids(sp_client):
+    results = sp_client.current_user_recently_played(limit=50)
+    id_list = []
+    for idx, res in enumerate(results['items']):
+        id_list.append({'spotify_id':res['track']['id'], 'name': res['track']['name'], 'artist': res['track']['artists'][0]['name'], 'played_at':res['played_at']})
+    return id_list
+
+
 if __name__ == '__main__':
     scope = "user-read-recently-played"
     CLIENT_ID, CLIENT_SECRET = creds.get_client_credentials()
     DBNAME, USER, PASSWORD, HOST, PORT = creds.get_db_credentials()    
     
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri="http://localhost:8080/callback",scope=scope))
-   
-    results = sp.current_user_recently_played(limit=5)
+    
 
-  
-    for idx, res in enumerate(results['items']):
-        print(res['track']['name'])
-        print(res['track']['artists'][0]['name'])
-        print(res['played_at'])
-        print("********************************************")
 
-   
+    id_list = get_recently_played_track_ids(sp)
+    print(id_list)
+
+#    for idx, res in enumerate(results['items']):
+#        id_list.append(res['track']['id'])
+#        print(res['track']['name'])
+#        print(res['track']['artists'][0]['name'])
+#        print(res['played_at'])
+#        print("********************************************")
+
+    #features =  sp.audio_features(id_list)
+    #print(features[0])
+    #print("*************************************************")
+    #print(features[1])
     db = Database(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
     db.create_tables()
     #db.insert("Breezeblocks", "Alt-J")
-    db.query("songs")
-    db.drop("songs")
+    #songs = db.query("songs")
+    #print(songs[0][0])
+    #db.drop("songs")
 
