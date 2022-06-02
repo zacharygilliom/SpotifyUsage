649a04e04c88cccaff90d651e2936b055ec18e3d
zacharygilliom <zacharygilliom@gmail.com>
2022-06-01 23:24:13 -0400

    added table insert statements for recently played tracks, added query to get tracks with no audio features.

---
+++ spotify/database.py
@@ -0,0 +1,91 @@
+import psycopg
+
+class Database:
+
+    def __init__(self, dbname, user, password, host, port):
+        self.params = {
+            'dbname': dbname,
+            'user': user,
+            'password': password,
+            'host': host,
+            'port': port
+        }
+        self.create_tables()
+
+    def create_tables(self):
+        with psycopg.connect(**self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS songs (
+                        id serial PRIMARY KEY,
+                        name varchar(100),
+                        artist varchar(100),
+                        spotify_id varchar(100) unique)
+                    """)
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS played (
+                        spotify_id varchar(100) references songs(spotify_id),
+                        played_at timestamp unique)
+                    """)
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS audio_features (
+                        spotify_id varchar(100) references songs(spotify_id) unique,
+                        danceability real,
+                        energy real,
+                        key integer,
+                        loudness real,
+                        mode int,
+                        speechiness real,
+                        acousticness real,
+                        instruementalness real,
+                        liveness real,
+                        valence real,
+                        tempo real)
+                    """)
+                conn.commit()
+                conn.close()
+
+    def query(self, table):
+        with psycopg.connect(**self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM {table}")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+                conn.commit()
+                conn.close()
+                return rows
+
+    def query_songs_no_features(self):
+        with psycopg.connect(**self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""SELECT spotify_id FROM audio_features WHERE danceability is NULL""")
+                rows = cur.fetchall()
+                return [row[0] for row in rows]
+
+
+    def insert_new_songs(self, song_list):
+        with psycopg.connect(**self.params) as conn:
+            with conn.cursor() as cur:
+                for song in song_list:
+                    cur.execute("""INSERT INTO songs (name, artist, spotify_id)
+                                    VALUES(%s,%s, %s)
+                                    ON CONFLICT (spotify_id) DO NOTHING""",
+                                    (song['name'], song['artist'], song['spotify_id']))
+                    cur.execute("""INSERT INTO audio_features (spotify_id)
+                                    VALUES(%s)
+                                    ON CONFLICT (spotify_id) DO NOTHING""",
+                                    (song['spotify_id'],))
+                    cur.execute("""INSERT INTO played (spotify_id, played_at)
+                                    VALUES (%s, %s)
+                                    ON CONFLICT (played_at) DO NOTHING""",
+                                    (song['spotify_id'], song['played_at']))
+                conn.commit()
+                conn.close()
+
+    def drop(self, table):
+        with psycopg.connect(**self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"DROP TABLE {table}")
+                conn.commit()
+                conn.close()
--- spotify/retrieve.py
+++ spotify/retrieve.py
@@ -1,116 +1,40 @@
 import spotipy
 import sys
-sys.path.append('/home/zach/programming/python/SpotifyUsage/internal')
-import creds 
+import os
+sys.path.append(f'{os.getcwd()}/internal')
+import creds
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
+from database import Database
 import psycopg
 
-class Database:
-
-    def __init__(self, dbname, user, password, host, port):
-        self.params = {
-            'dbname': dbname,
-            'user': user,
-            'password': password,
-            'host': host,
-            'port': port 
-        }
-
-    def create_tables(self):
-        with psycopg.connect(**self.params) as conn:
-            with conn.cursor() as cur:
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS songs (
-                        id serial PRIMARY KEY,
-                        name varchar(20),
-                        band varchar(20),
-                        spotify_id varchar(22))
-                    """)
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS played (
-                        song_id integer references songs(id),
-                        played_at timestamp)
-                    """)
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS audio_features (
-                        song_id integer references songs(id),
-                        danceability real,
-                        energy real,
-                        key integer,
-                        loudness real,
-                        mode int,
-                        speechiness real,
-                        acousticness real,
-                        instruementalness real,
-                        liveness real,
-                        valence real,
-                        tempo real)
-                    """)
-                conn.commit()
-                conn.close()
-
-    def query(self, db):
-        with psycopg.connect(**self.params) as conn:
-            with conn.cursor() as cur:
-                cur.execute(f"SELECT * FROM {db}")
-                rows = cur.fetchall()
-                for row in rows:
-                    print(row)
-                conn.commit()
-                conn.close()
-                return rows
-
-    def insert(self, song, band):
-        with psycopg.connect(**self.params) as conn:
-            with conn.cursor() as cur:
-                cur.execute("INSERT INTO songs (name,band) VALUES(%s,%s)",
-                    (song, band))
-                conn.commit()
-                conn.close()
-
-    def drop(self, table):
-        with psycopg.connect(**self.params) as conn:
-            with conn.cursor() as cur:
-                cur.execute(f"DROP TABLE {table}")
-                conn.commit()
-                conn.close()
-
-def get_recently_played_track_ids(sp_client):
+def get_recently_played_tracks(sp_client, db):
     results = sp_client.current_user_recently_played(limit=50)
     id_list = []
     for idx, res in enumerate(results['items']):
         id_list.append({'spotify_id':res['track']['id'], 'name': res['track']['name'], 'artist': res['track']['artists'][0]['name'], 'played_at':res['played_at']})
-    return id_list
+    db.insert_new_songs(id_list)
 
+def get_recently_played_audio_features(sp_client, db):
+    no_features_list = db.query_songs_no_features()
+    results = sp_client.audio_features(no_features_list)
+    return null
 
 if __name__ == '__main__':
+
     scope = "user-read-recently-played"
     CLIENT_ID, CLIENT_SECRET = creds.get_client_credentials()
     DBNAME, USER, PASSWORD, HOST, PORT = creds.get_db_credentials()    
     
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri="http://localhost:8080/callback",scope=scope))
     
-
-
-    id_list = get_recently_played_track_ids(sp)
-    print(id_list)
-
-#    for idx, res in enumerate(results['items']):
-#        id_list.append(res['track']['id'])
-#        print(res['track']['name'])
-#        print(res['track']['artists'][0]['name'])
-#        print(res['played_at'])
-#        print("********************************************")
-
     #features =  sp.audio_features(id_list)
     #print(features[0])
     #print("*************************************************")
     #print(features[1])
     db = Database(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
-    db.create_tables()
+    get_recently_played_tracks(sp, db)
+    get_recently_played_audio_features(sp, db)
+    #db.insert_songs(id_list)
     #db.insert("Breezeblocks", "Alt-J")
-    #songs = db.query("songs")
-    #print(songs[0][0])
     #db.drop("songs")
-
